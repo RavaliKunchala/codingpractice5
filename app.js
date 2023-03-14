043425e3b3e19f6b13774173274dc74ff6d5f330
Ravali Kunchala <Enter you email ID here>
2023-03-14 05:45:37 +0000

    example

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const app = express();
+
+const dbPath = path.join(__dirname, "cricketTeam.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+//GET states
+app.get("/states/", async (request, response) => {
+  const getStatesQuery = `
+    SELECT
+      *
+    FROM
+     State
+    ORDER BY
+   state_id;`;
+  const teamArray = await db.all(getStatesQuery);
+  response.send(teamArray);
+});
